6df27b9387cdf96fd107accde5b778ecc6740764
Milenkovic, Djordje <djordje.milenkovic@fisglobal.com>
2026-04-14 15:47:36 +0200

    fix fillings and reports pages display

--- src/Rentier.Desktop/Converters/InvertBoolConverter.cs
+++ src/Rentier.Desktop/Converters/InvertBoolConverter.cs
@@ -5,5 +5,5 @@ namespace Rentier.Desktop.Converters;
 public static class InvertBoolConverter
 {
     public static readonly IValueConverter Instance =
-        new FuncValueConverter<bool, bool>(b => !b);
+        new FuncValueConverter<bool, bool>(b => !b, b => !b);
 }
